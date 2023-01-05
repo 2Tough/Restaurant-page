0226a2e5804b26fa768df9ca0e191b47130ba85f
Polaski <vainferro@gmail.com>
2023-01-05 21:10:34 +0300

    Added new barebones structure to page.

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,14 @@
 export function contact() {
-    
+
+    const contentDiv = document.querySelector("#content");
+
+    contentDiv.replaceChildren();
+
+    const heading = document.createElement("h1");
+    heading.textContent = "For questions or reservations, please contact us at:";
+    heading.classList.add("content--heading");
+    contentDiv.appendChild(heading);
+
+
+
 }
